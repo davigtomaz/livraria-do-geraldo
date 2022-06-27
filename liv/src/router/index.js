3e5b8fe5d10c834821ece2a1c6aba01db8a14b7a
Davibrlol <94870528+Davibrlol@users.noreply.github.com>
2022-06-27 14:49:32 -0300

    Add files via upload

--- liv/src/router/index.js
+++ liv/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CadastrosView from "../views/CadastrosView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/cadastro',
+      name: 'Cadastro',
+      component: CadastrosView
     },
   ]
 })
